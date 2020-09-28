7c1ce63616653be076540accbb8d5eaaf165f49d
Jamie Howarth <hello@jamiehowarth.me>
2020-09-29 00:15:28 +0100

    Update IHCaptchaApi.cs
    Changed the Refit path to be a string because constants aren't supported by Refit (yet)

--- src/HCaptcha/IHCaptchaApi.cs
+++ src/HCaptcha/IHCaptchaApi.cs
@@ -12,7 +12,7 @@ namespace BenjaminAbt.HCaptcha
         /// <summary>
         /// The endpoint expects a POST request with two parameters: your <paramref name="secret"/> API key and the h-captcha-response token (<paramref name="response"/> POSTed from your HTML page. You can optionally include the user's IP address (<paramref name="remoteIp"/>) as an additional security check. Do not send JSON data: the endpoint expects a standard URL-encoded form POST.
         /// </summary>
-        [Post(HCaptchaOptions.VerifyPath)]
+        [Post("/siteverify")]
         Task<HCaptchaVerifyResponse> Verify(string secret, string response, string remoteIp = null,
             CancellationToken cancellationToken = default);
     }
